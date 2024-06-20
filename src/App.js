65de65c43d392cbcf915fb6e16cecb6762c38fe9
Anastasia H. <anastasia.hrynkevich@outlook.com>
2024-06-20 20:36:44 +0100

    started creating the "navBar", still working on it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import React from "react";
+import {
+    RouterProvider,
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route
+} from "react-router-dom";
+
+// pages
+import Home from  "./pages/Home";
+
+// layouts
+import RootLayout from "./layouts/RootLayout";
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<RootLayout />}>
+            <Route index element={<Home />} />
+        </Route>
+    )
+)
+
+
 export default function App() {
   return (
     <>
-      <h1>Hello World!</h1>
+        <RouterProvider router={router} />
     </>
   );
 }
---
+++ src/layouts/RootLayout.js
@@ -0,0 +1,9 @@
+export default function RootLayout() {
+    return (
+        <>
+            <header>
+                <h1>Home</h1>
+            </header>
+        </>
+    )
+}
