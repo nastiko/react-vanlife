542c159c5ef81c37b20f1203f0cf3c028d8727dd
Anastasia H. <anastasia.hrynkevich@outlook.com>
2024-07-23 22:45:51 +0100

    changed homepage path for relative ulr support. attempt #2

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ import { requireAuth } from "./utils";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
-        <Route element={<RootLayout/>}>
+        <Route path={window.location.pathname} element={<RootLayout/>}>
             <Route index element={<Home/>}/>
 
             <Route>
