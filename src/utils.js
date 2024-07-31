3245b1b57f4ef2869cf1ec17e4350c9e2b31f3aa
Anastasia H. <anastasia.hrynkevich@outlook.com>
2024-07-31 20:27:37 +0100

    changed path for protected link - test

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,11 @@
 import { redirect } from "react-router-dom";
 
 export async function requireAuth(request) {
-    const pathname = `${process.env.PUBLIC_URL}${new URL(request.url).pathname}`;
+    const pathname = new URL(request.url).pathname;
     const isLoggedIn = localStorage.getItem("loggedin");
 
     if (!isLoggedIn) {
-        const response = redirect(`/login?message=You must log in first.&redirectTo=${pathname}`);
+        const response = redirect(`${process.env.PUBLIC_URL}/login?message=You must log in first.&redirectTo=${pathname}`);
         response.body = true;
         return response;
     }
